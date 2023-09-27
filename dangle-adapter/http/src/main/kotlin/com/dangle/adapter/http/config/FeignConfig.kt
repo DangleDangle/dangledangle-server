5e325c65b52777343010ff0086c192f8d2c00475
kang <rilac1@naver.com>
2023-09-27 23:17:20 +0900

    YW2-224 feat: HTTP 모듈 빈 설정 적용

---
+++ dangle-adapter/http/src/main/kotlin/com/dangle/adapter/http/config/FeignConfig.kt
@@ -0,0 +1,12 @@
+package com.dangle.adapter.http.config
+
+import com.dangle.adapter.http.kakao.KakaoNotificationClient
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.openfeign.FeignClientProperties
+
+@EnableFeignClients(clients = [
+    KakaoNotificationClient::class,
+])
+@EnableConfigurationProperties(FeignClientProperties::class)
+class FeignConfig
