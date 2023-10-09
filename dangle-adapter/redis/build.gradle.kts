00511a57e7f04f8ef5b468f9aaeeb6e14f07ad58
Jeonghyun Kang <rilac1@naver.com>
2023-10-09 16:00:20 +0900

    YW2-243: feat: EmbeddedRedis 설정

--- dangle-adapter/redis/build.gradle.kts
+++ dangle-adapter/redis/build.gradle.kts
@@ -3,6 +3,9 @@ dependencies {
     implementation(project(":dangle-domain"))
 
     api("org.springframework.boot:spring-boot-starter-data-redis")
+    api("it.ozimov:embedded-redis:0.7.3") {
+        exclude(group = "org.slf4j", module = "slf4j-simple")
+    }
 }
 
 tasks.bootJar { enabled = false }
--- dangle-common/src/test/kotlin/com/dangle/common/DangleCommonApplicationTests.kt
+++ dangle-common/src/test/kotlin/com/dangle/common/DangleCommonApplicationTests.kt
@@ -1,13 +1,6 @@
 package com.dangle.common
 
-import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DangleCommonApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
+class DangleCommonApplicationTests
