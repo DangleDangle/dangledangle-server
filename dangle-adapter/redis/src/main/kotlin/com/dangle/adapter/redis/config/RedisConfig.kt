43d52671ff704732398bbceb388b97f15fbb1b8a
Jeonghyun Kang <rilac1@naver.com>
2023-10-09 16:00:17 +0900

    YW2-243 feat: 레디스 Config 수정

--- dangle-adapter/redis/src/main/kotlin/com/dangle/adapter/redis/config/RedisConfig.kt
+++ dangle-adapter/redis/src/main/kotlin/com/dangle/adapter/redis/config/RedisConfig.kt
@@ -1,32 +1,14 @@
 package com.dangle.adapter.redis.config
 
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.connection.RedisPassword
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
-class RedisConfig (
-    private val redisProperties: RedisProperties,
-) {
-    @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory(
-            RedisStandaloneConfiguration(
-                redisProperties.host,
-                redisProperties.port,
-            ).apply {
-                this.password = RedisPassword.of(redisProperties.password)
-            }
-        )
-    }
-
+class RedisConfig {
     @Bean
     fun redisTemplate(
         connectionFactory: RedisConnectionFactory,
