9ae9a71a0275e26b915b250b0e4974a02001ad95
Jeonghyun Kang <rilac1@naver.com>
2023-10-14 16:55:17 +0900

    YW2-238 feat: ktlint 수정

--- dangle-adapter/persistence/src/main/kotlin/com/dangle/adapter/persistence/entity/ShelterBookMarkEntity.kt
+++ dangle-adapter/persistence/src/main/kotlin/com/dangle/adapter/persistence/entity/ShelterBookMarkEntity.kt
@@ -1,6 +1,5 @@
 package com.dangle.adapter.persistence.entity
 
-import com.dangle.adapter.persistence.common.BaseTimeEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
